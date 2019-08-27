a2faa540143dc43892f9d05930a371196130d114
Aliaksei Danilevich <aliaksei.danilevich@thomsonreuters.com>
2019-08-27 19:15:44 +0300

    Init

---
+++ BaseApi/ExpenseTracker.API/Controllers/ExpenseGroupsController.cs
@@ -0,0 +1,303 @@
+﻿using ExpenseTracker.Repository;
+using ExpenseTracker.Repository.Factories;
+using System;
+using Marvin.JsonPatch;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+using ExpenseTracker.API.Controllers.Helpers;
+using System.Web;
+using ExpenseTracker.DTO;
+
+namespace ExpenseTracker.API.Controllers
+{
+    public class ExpenseGroupsController : ApiController
+    {
+        IExpenseTrackerRepository _repository;
+        ExpenseGroupFactory _expenseGroupFactory = new ExpenseGroupFactory();
+
+        private const int maxPageSize = 10;
+
+        public ExpenseGroupsController()
+        {
+            _repository = new ExpenseTrackerEFRepository(new
+                Repository.Entities.ExpenseTrackerContext());
+        }
+
+        public ExpenseGroupsController(IExpenseTrackerRepository repository)
+        {
+            _repository = repository;
+        }
+
+        //[Route("expensegroups")] //Multiple actions were found that match 
+        //public IHttpActionResult Get()
+        //{
+        //    try
+        //    {
+        //        var expenseGroups = _repository.GetExpenseGroups();
+
+        //        return Ok(expenseGroups.ToList()
+        //            .Select(eg => _expenseGroupFactory.CreateExpenseGroup(eg)));
+
+        //    }
+        //    catch (Exception)
+        //    {
+        //        return InternalServerError();
+        //    }
+        //}
+
+        // http://localhost:43321/api/expensegroups?sort=-expenseGroupStatusId,title
+        //http://localhost:43321/api/expensegroups?status=open&sort=-title
+        //http://localhost:43321/api/expensegroups?page=2&pagesize=2
+        [Route("api/expensegroups", Name = "ExpenseGroupsList")]
+        public IHttpActionResult Get(string sort = "id", string status = null, string userId = null, int page = 1, int pageSize = maxPageSize)
+        {
+            try
+            {
+                int statusId = -1;
+                if (status != null)
+                {
+                    switch (status.ToLower())
+                    {
+                        case "open":
+                            statusId = 1;
+                            break;
+                        case "confirmed":
+                            statusId = 2;
+                            break;
+                        case "processed":
+                            statusId = 3;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+
+                // get expensegroups from repository
+                var expenseGroups = _repository.GetExpenseGroups()
+                    .ApplySort(sort)
+                    .Where(eg => (statusId == -1 || eg.ExpenseGroupStatusId == statusId))
+                    .Where(eg => (userId == null || eg.UserId == userId));
+
+                // ensure the page size isn't larger than the maximum.
+                if (pageSize > maxPageSize)
+                {
+                    pageSize = maxPageSize;
+                }
+
+                // calculate data for metadata
+                var totalCount = expenseGroups.Count();
+                var totalPages = (int)Math.Ceiling((double)totalCount / pageSize);
+
+                var urlHelper = new UrlHelper(Request);
+                var prevLink = page > 1 ? urlHelper.Link("ExpenseGroupsList",
+                    new
+                    {
+                        page = page - 1,
+                        pageSize = pageSize,
+                        sort = sort
+                        ,
+                        status = status,
+                        userId = userId
+                    }) : "";
+                var nextLink = page < totalPages ? urlHelper.Link("ExpenseGroupsList",
+                    new
+                    {
+                        page = page + 1,
+                        pageSize = pageSize,
+                        sort = sort
+                        ,
+                        status = status,
+                        userId = userId
+                    }) : "";
+
+
+                var paginationHeader = new
+                {
+                    currentPage = page,
+                    pageSize = pageSize,
+                    totalCount = totalCount,
+                    totalPages = totalPages,
+                    previousPageLink = prevLink,
+                    nextPageLink = nextLink
+                };
+
+                HttpContext.Current.Response.Headers.Add("X-Pagination",
+                    Newtonsoft.Json.JsonConvert.SerializeObject(paginationHeader));
+
+                // return result
+                return Ok(expenseGroups
+                    .Skip(pageSize * (page - 1))
+                    .Take(pageSize)
+                    .ToList()
+                    .Select(eg => _expenseGroupFactory.CreateExpenseGroup(eg)));
+
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+
+        public IHttpActionResult Get(int id)
+        {
+            try
+            {
+                var expenseGroup = _repository.GetExpenseGroup(id);
+
+                if (expenseGroup != null)
+                {
+                    return Ok(_expenseGroupFactory.CreateExpenseGroup(expenseGroup));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] DTO.ExpenseGroup expenseGroup)
+        {
+            try
+            {
+                if (expenseGroup == null)
+                {
+                    return BadRequest();
+                }
+
+                var eg = _expenseGroupFactory.CreateExpenseGroup(expenseGroup);
+                var result = _repository.InsertExpenseGroup(eg);
+
+                if (result.Status == RepositoryActionStatus.Created)
+                {
+                    var newExpenseGroup = _expenseGroupFactory.CreateExpenseGroup
+                        (result.Entity);
+
+                    return Created(Request.RequestUri + "/" + newExpenseGroup.Id.ToString()
+                        , newExpenseGroup);
+                }
+
+                return BadRequest();
+
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+        public IHttpActionResult Put(int id, [FromBody]DTO.ExpenseGroup expenseGroup)
+        {
+            try
+            {
+                if (expenseGroup == null)
+                    return BadRequest();
+
+                // map
+                var eg = _expenseGroupFactory.CreateExpenseGroup(expenseGroup);
+
+                var result = _repository.UpdateExpenseGroup(eg);
+                if (result.Status == RepositoryActionStatus.Updated)
+                {
+                    // map to dto
+                    var updatedExpenseGroup = _expenseGroupFactory.CreateExpenseGroup(result.Entity);
+
+                    return Ok(updatedExpenseGroup);
+                }
+                else if (result.Status == RepositoryActionStatus.NotFound)
+                {
+                    return NotFound();
+                }
+
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+
+        }
+
+        // install Marvin.JsonPatch
+        /*  [
+            {"op":"replace", "path":"/title", "value":"New Title 2"},
+            {"op":"copy", "from":"/title", "path":"/description"}
+            ]
+        */
+        [HttpPatch]
+        public IHttpActionResult Patch(int id, [FromBody]JsonPatchDocument<DTO.ExpenseGroup> expenseGroupPatchDocument)
+        {
+            try
+            {
+                if (expenseGroupPatchDocument == null)
+                {
+                    return BadRequest();
+                }
+
+                var expenseGroup = _repository.GetExpenseGroup(id);
+                if (expenseGroup == null)
+                {
+                    return NotFound();
+                }
+
+                // map
+                var eg = _expenseGroupFactory.CreateExpenseGroup(expenseGroup);
+
+                // apply changes to the DTO
+                expenseGroupPatchDocument.ApplyTo(eg);
+
+                // map the DTO with applied changes to the entity, & update
+                var result = _repository.UpdateExpenseGroup(_expenseGroupFactory.CreateExpenseGroup(eg));
+
+                if (result.Status == RepositoryActionStatus.Updated)
+                {
+                    // map to dto
+                    var patchedExpenseGroup = _expenseGroupFactory.CreateExpenseGroup(result.Entity);
+                    return Ok(patchedExpenseGroup);
+                }
+
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+
+
+
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+
+                var result = _repository.DeleteExpenseGroup(id);
+
+                if (result.Status == RepositoryActionStatus.Deleted)
+                {
+                    return StatusCode(HttpStatusCode.NoContent);
+                }
+                else if (result.Status == RepositoryActionStatus.NotFound)
+                {
+                    return NotFound();
+                }
+
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+        }
+    }
+}
